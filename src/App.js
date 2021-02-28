80938478090850453b317fb3d4a265e35422e771
Samantha Wessel <samantha.n.wessel@gmail.com>
2021-02-27 22:58:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from 'react-dom';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.scss';
+import Headphones from './assets/Headphones';
 
-export default App;
+
+const App = () => 
+     (
+        <div className="title-container">
+            <h1>Taylore</h1>
+            <Headphones/>
+        </div>
+    )
+;
+
+render(<App></App>, document.getElementById('root'));
